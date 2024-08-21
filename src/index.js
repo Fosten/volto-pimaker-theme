5fb2d20c610bced03f6f90966d39494c00c6a12e
Fosten <fosten@gmail.com>
2024-08-21 15:16:57 -0400

    Use navroot in title tag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const applyConfig = (config) => {
+  //include site title in <title>
+  if (!config.settings.siteTitleFormat) {
+    config.settings.siteTitleFormat = { includeSiteTitle: true };
+  } else config.settings.siteTitleFormat.includeSiteTitle = true;
+  config.settings.titleAndSiteTitleSeparator = '|';
+
   return config;
 };
 
